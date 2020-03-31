15e854afa7073387cef228fe55ad51850f2b1c78
efebirol <tw04n202@gmail.com>
2020-04-01 01:34:41 +0200

    Property und Setter Injection

--- src/main/java/com/example/demo/controller/MyCarController.java
+++ src/main/java/com/example/demo/controller/MyCarController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,11 @@ import com.example.demo.entity.CarEngine;
 import lombok.extern.slf4j.Slf4j;
 
 
+/**
+ * Handhabung: Einfach den Haupttest ausführen und die Logs anschauen
+ * 
+ * @author Efe
+ */
 @RestController
 @RequestMapping("/mycontroller")
 @Slf4j
@@ -21,12 +27,40 @@ public class MyCarController
     log.info("Konstruktor");
   }
 
-  // Property/Field injection
+  /*
+   * Property/Field injection
+   * "CarEngine"-Component-Klasse wird hier eingebunden in die Controller
+   */
   @Autowired
   CarEngine engine;
 
+  CarEngine setterInjEngine;
 
-  // Setter Injection
+  /*
+   * Setter Injection
+   * "CarEngine"-Component-Klasse wird hier eingebunden in die Controller
+   */
+  @Autowired
+  private void setCarEngine(CarEngine engineProp)
+  {
+    log.info("Setter Injection für die CarEngine-Klasse");
+    this.setterInjEngine = engineProp;
+  }
+
+  /**
+   * Show usage of Property and Setter Injection
+   * Hinweis: Er wird nur eine "CarEngine"-Bean im SpringContext angelegt und von beiden Methoden benutzt/überschrieben
+   */
+  @GetMapping(value = "/auto")
+  public void showCarsBeans()
+  {
+    log.info("Property/Field injection:");
+    log.info("- Car: {}", engine.getEngineName());
+
+    log.info("Setter Injection:");
+    setterInjEngine.setEngineName("FordEngine");
+    log.info("- Car: {}", setterInjEngine.getEngineName());
+  }
 
   // Constructor injection
 
--- src/main/java/com/example/demo/entity/CarEngine.java
+++ src/main/java/com/example/demo/entity/CarEngine.java
@@ -2,14 +2,23 @@ package com.example.demo.entity;
 
 import org.springframework.stereotype.Component;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 
 @Component
 @Slf4j
+@Getter
+@Setter
 public class CarEngine
 {
 
+  String engineName = "v8";
+
+  /**
+   * Wird über den "MyCarController" eingebunden
+   */
   public CarEngine()
   {
     log.info("Konstruktor");
