260a6a33a4fa78ab4ba6f6d8445788f7982b1737
efebirol <tw04n202@gmail.com>
2020-04-01 02:00:39 +0200

    Spring Bean Übersicht ausgeben

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,48 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import lombok.extern.slf4j.Slf4j;
+
 
 @SpringBootApplication
-public class DemoApplication {
+@Slf4j
+public class DemoApplication implements CommandLineRunner
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+  private static ApplicationContext appContext;
 
+  public static void main(String[] args)
+  {
+    SpringApplication.run(DemoApplication.class, args);
+
+  }
+
+  // This will print all the beans from applicationContext container:
+
+  @Bean
+  public CommandLineRunner run(ApplicationContext appContext)
+  {
+    return args -> {
+
+      String[] beans = appContext.getBeanDefinitionNames();
+      log.info("Spring Bean Übersicht - Start");
+      Arrays.stream(beans).sorted().forEach(System.out::println);
+      log.info("Spring Bean Übersicht - Ende");
+
+    };
+  }
+
+  @Override
+  public void run(String... args) throws Exception
+  {
+    // TODO Auto-generated method stub
+
+  }
 }
